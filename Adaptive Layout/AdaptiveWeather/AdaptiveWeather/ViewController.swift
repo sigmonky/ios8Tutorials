a42ce984747ca253017f9ba45420475081d5b791
sigmonky <sigmonky@gmail.com>
2014-09-29 18:01:04 -0400

    fist commit

---
+++ Adaptive Layout/AdaptiveWeather/AdaptiveWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdaptiveWeather
+//
+//  Created by Weinstein, Randy - Nick.com on 9/29/14.
+//  Copyright (c) 2014 fakeancient. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
